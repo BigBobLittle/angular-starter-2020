c22215ec9ffb41bf2511bf0d18d624b629a610d5
BigBobLittle <mac@BigBobLittle.local>
2020-10-06 09:48:41 +0000

    setting up... added widgets module to app

---
+++ src/app/_widgets/widgets/widgets.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@NgModule({
+  declarations: [NavComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [NavComponent, FooterComponent],
+})
+export class WidgetsModule {}
